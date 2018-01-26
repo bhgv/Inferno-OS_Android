d6ca5c89d74854db9ed8bd40b29ccc9edaff1222
bhgv <bhgv.empire@gmail.com>
2018-01-27 00:17:22 +0200

    new icons and some checks

--- inferno-os/cln.bat
+++ inferno-os/cln.bat
@@ -1,12 +1,12 @@
 
 ::set NDK=D:\Android\sdk\ndk-bundle
 
-set PWD=C:\Users\x200t\AndroidStudioProjects\inferno_os\inferno-os
+set PWD=C:/Users/x200t/AndroidStudioProjects/inferno_os/inferno-os
 
 ::python %NDK%/build/tools/make_standalone_toolchain.py --arch arm --api 17 --install-dir=ndk_toolchain
 
 
-set PATH=%PWD%\Nt\386\bin;%PWD%\ndk_toolchain\bin;%PATH%
+set PATH=%PWD%/Nt/386/bin;%PWD%/ndk_toolchain/bin;%PATH%
 
 ::call "C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\vcvarsall.bat" x86
 
